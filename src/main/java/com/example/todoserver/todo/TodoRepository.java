7408aefe8ad7c18c5ce93b00a50da5e7f773c371
suhasinipatil <suhasinibpatil09@gmail.com>
2023-04-23 17:15:40 +0530

    added usercontroller

--- src/main/java/com/example/todoserver/todo/TodoRepository.java
+++ src/main/java/com/example/todoserver/todo/TodoRepository.java
@@ -3,10 +3,14 @@ package com.example.todoserver.todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
 
     @Override
     void deleteById(Integer todoId);
 
+    @Override
+    List<TodoEntity> findAll();
 }
---
+++ src/main/java/com/example/todoserver/user/UserController.java
@@ -0,0 +1,27 @@
+package com.example.todoserver.user;
+
+import com.example.todoserver.user.dto.CreateUserDTO;
+import com.example.todoserver.user.dto.UserResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody CreateUserDTO createUserDTO) throws URISyntaxException {
+        var createdUser = userService.createUser(createUserDTO);
+        return ResponseEntity.created(new URI("/login/" + createdUser.getId())).body(createdUser);
+    }
+}
